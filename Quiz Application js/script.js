7e0384d01ad782b8c5732dff045b461d4bf9dd48
Muhammad Usman <61974138+usmandevops123@users.noreply.github.com>
2021-01-12 04:29:41 -0800

    Upload

---
+++ Quiz Application js/script.js
@@ -0,0 +1,57 @@
+//console.log('Welcome to quiz app');
+const quizdata=[
+    {
+        1. Which of the following is not JavaScript Data Types?
+
+        A. Undefined
+        B. Number
+        C. Boolean
+        D. Float 
+}, 
+{
+    2. Which company developed JavaScript?
+
+A. Netscape
+B. Bell Labs
+C. Sun Microsystems
+D. IBM 
+
+},
+{
+    3. Inside which HTML element do we put the JavaScript?
+
+A. <script>
+B. <head>
+C. <meta>
+D. <style>
+},
+{
+    4. Which of the following is correct about features of JavaScript?
+
+A. It can not Handling dates and time.
+B. JavaScript is a object-based scripting language.
+C. JavaScript is not interpreter based scripting language.
+D. All of the above 
+}, 
+{
+    5. Choose the correct JavaScript syntax to change the content of the following HTML code.
+
+A. document.getElement (“letsfindcourse").innerHTML = "I am a letsfindcourse";
+B. document.getElementById (“letsfindcourse").innerHTML = "I am a letsfindcourse";
+C. document.getId (“letsfindcourse") = "I am a letsfindcourse";
+D. document.getElementById (“letsfindcourse").innerHTML = I am a letsfindcourse; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
